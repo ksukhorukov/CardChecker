1706b19d08c9d2dd93c70fc62d79611c5f015924
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-01-18 12:31:26 +0300

    card type validation method. refactoring part 3

--- lib/card.rb
+++ lib/card.rb
@@ -13,13 +13,13 @@ class Card
   end
 
   def type
-    return 'AMEX' if card_length_is 15 and (prefix_of_card_equal? '34' or prefix_of_card_equal? '37')
+    return 'AMEX' if is_amex?
      
-    return 'Discover' if card_length_is 16 and prefix_of_card_equal? '6011'
+    return 'Discover' if is_discover?
       
-    return 'MasterCard' if card_length_is 16 and (first_two_card_digits >= 51 and first_two_card_digits <= 55)
+    return 'MasterCard' if is_mastercard?
       
-    return 'Visa' if card_length_is_or 13, 16 and prefix_of_card_equal? '4'
+    return 'Visa' if is_visa?
       
     'Unknown'
   end
@@ -65,5 +65,21 @@ class Card
 
   def card_length_is_or(n, m)
     [n, m].include?(card_length)
+  end 
+
+  def is_amex?
+    card_length_is 15 and (prefix_of_card_equal? '34' or prefix_of_card_equal? '37')
+  end 
+
+  def is_discover?
+    card_length_is 16 and prefix_of_card_equal? '6011'
+  end 
+
+  def is_mastercard?
+    card_length_is 16 and (first_two_card_digits >= 51 and first_two_card_digits <= 55)
+  end 
+
+  def is_visa?
+    card_length_is_or 13, 16 and prefix_of_card_equal? '4'
   end 
 end
