3fef2e9c814eda875fbd0b49c202618509d94a64
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-01-18 12:03:16 +0300

    refactoring of the initializer

--- lib/card.rb
+++ lib/card.rb
@@ -1,19 +1,15 @@
+#!/usr/bin/env ruby
+
 class Card
-  
   attr_reader :card
 
+  ERROR_INVALID_CARD_FORMAT = 'Card number must consists of digits'
+  ERROR_INVALID_CARD_LENGTH = 'Length of a card must be between 13 and 16'
+
   def initialize(number)
-
-    unless number.match('^\d+$')
-      raise 'Card number must consists of digets'
-    end
-
-    if number.length < 13 or number.length > 16
-      raise 'Length of a card must be between 13 and 16'
-    end
-
     @card = number
 
+    validation
   end
 
   def type
@@ -22,9 +18,9 @@ class Card
       'AMEX'
     elsif card_length == 16 and @card[0..3] == '6011'
       'Discover'
-    elsif  card_length == 16 and (@card[0..1].to_i >= 51 and @card[0..1].to_i <= 55)
+    elsif card_length == 16 and (@card[0..1].to_i >= 51 and @card[0..1].to_i <= 55)
       'MasterCard'
-    elsif (card_length == 13 or card_length == 16) and @card[0] == '4'
+    elsif [13, 16].include?(card_length) and @card[0] == '4'
       'Visa'
     else
       'Unknown'
@@ -34,17 +30,27 @@ class Card
   def valid?
     summ = 0
     @card.split('').map(&:to_i).reverse.each_with_index do |n, i|
-      if (i+1).even?
+      if (i + 1).even?
         n *= 2
         n = n.to_s.split('').map(&:to_i).reduce(&:+) if n > 9
       end
-      summ += n 
-    end 
-    if summ % 10 == 0
-      true
-    else
-      false
+      summ += n
     end
+    summ % 10 == 0
   end
 
+  private 
+
+  def validation 
+    raise ERROR_INVALID_CARD_FORMAT unless card_number_consists_of_digits?
+    raise ERROR_INVALID_CARD_LENGTH if card_length_acceptable?
+  end 
+
+  def card_number_consists_of_digits?
+    card.match('^\d+$')
+  end 
+
+  def card_length_acceptable?
+    card.length < 13 or card.length > 16
+  end 
 end
