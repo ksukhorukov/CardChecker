4aca8a7c6ba101389c9fb5776467b787a68cc940
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-19 02:49:37 +0800

    refactoting part 2

--- lib/types.rb
+++ lib/types.rb
@@ -1,43 +1,7 @@
 require_relative 'bank_types'
+require_relative 'type_checker'
 
 class Cards
   include BankTypes
-  module Types
- 
-    def is_amex?
-      card_length_is 15 and (prefix_of_card_equal? '34' or prefix_of_card_equal? '37')
-    end
-
-    def is_discover?
-      card_length_is 16 and prefix_of_card_equal? '6011'
-    end
-
-    def is_mastercard?
-      card_length_is 16 and (first_two_card_digits >= 51 and first_two_card_digits <= 55)
-    end
-
-    def is_visa?
-      card_length_is_or 13, 16 and prefix_of_card_equal? '4'
-    end
-
-    def is_sber?
-      card_length_is_or 16 and prefix_of_card_equal? '2202'
-    end
-
-    def is_mir?
-      card_length_is_or 16 and prefix_of_card_equal? '220015'
-    end
-
-    def is_raifaizen?
-      card_length_is 16 and raifizen_prefix?
-    end
-
-    def raifizen_prefix?
-      RAIFAIZEN_TYPE_PREFIXES.map { |prefix| prefix_of_card_equal? prefix }.reduce(:or)  
-    end
-
-    def kaspi?
-      card_length_is_or 16 and prefix_of_card_equal? '440043'
-    end 
-  end
+  include TypeChecker
 end
